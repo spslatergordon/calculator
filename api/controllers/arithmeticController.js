5c1c1a144fc4b6a09c459dd10ad4cebc059b251f
spslatergordon <125361282+spslatergordon@users.noreply.github.com>
2023-02-14 16:07:33 +0000

    breaking change

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,7 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return +a + +b },
+    'add':      function(a,b) { return +a + +b + divide(1, 0) },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
